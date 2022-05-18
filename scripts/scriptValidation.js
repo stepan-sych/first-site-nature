af1e019b4676bbef2881e1bceb669f636ff1ad59
Stepan Sych <stepansych@gmail.com>
2022-05-18 19:41:16 +0300

    Fix disabled button

--- scripts/scriptValidation.js
+++ scripts/scriptValidation.js
@@ -1,72 +1,76 @@
-const EnableDisable = () => {
-    let elements = document.querySelectorAll("#name, #mail, #phone");
-    let submitBtn = false;
-    for (const el of elements) {
-        if (el.value.trim() === "") {
-            submitBtn = true;
-            break;
-        }
-    }
-    document.getElementById("submitBtn").disabled = submitBtn;
-}
-
 document.addEventListener("DOMContentLoaded", () => {
+    let elements = document.querySelectorAll("#name, #mail, #phone");
+    for (const element of elements) {
+        element.addEventListener("keyup", () => {
+            let submitBtn = false;
+            for (const el of elements) {
+                if (el.value.trim() === "") {
+                    submitBtn = true;
+                }
+                document.getElementById("submitBtn").disabled = submitBtn;
+            }
+        });
+
+    }
+
     const form = document.getElementById("form");
-    const formSend = async (e) => {
+    form.addEventListener("submit", async (e) => {
         e.preventDefault();
         let isValidForm = formValidate();
         if (isValidForm === true) {
             form.submit();
         }
-    }
-    form.addEventListener("submit", formSend);
+    });
 });
 
 const formValidate = () => {
     let isValid = true;
-    let formReq = document.querySelectorAll("#mail, #name, #phone"); //Todo leave class or ids
+    let formInputs = document.querySelectorAll("#mail, #name, #phone");
 
     const validateField = (field, checkFieldCallback) => {
-        if (checkFieldCallback(field)) {
+        if (checkFieldCallback(field.value)) {
             formAddError(field);
             isValid = false;
         } else {
             formRemoveError(field)
         }
     }
-    for (const inputTag of formReq) {
-        if (inputTag.classList.contains("-email")) {
-            validateField(inputTag, emailValidation);
-        } else if (inputTag.classList.contains("-name")) {
-            validateField(inputTag, nameValidation);
-        } else if (inputTag.classList.contains("-tel")) {
-            validateField(inputTag, telNumValidation);
-        } else if (inputTag.value === "") { //Todo remove
-            formAddError(inputTag);
-            isValid = false;
+    for (const inputTag of formInputs) {
+        console.log("inputTeg");
+        console.log(inputTag);
+        console.log("formInputs");
+        console.log(formInputs);
+        if (inputTag.id === "mail") {
+            validateField(inputTag, isEmailValidation); //todo rename
+        } else if (inputTag.id === "name") {
+            validateField(inputTag, isNameValidation);
+        } else if (inputTag.id === "phone") {
+            validateField(inputTag, isTelNumValidation);
         }
     }
     return isValid;
 }
 
+const isEmailValidation = (value) => {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(value);
+}
+
+const isNameValidation = (value) => {
+    return /^[0-9,!@#$%^&*)(_/?\-=.`'"|~\]\[₴№;:}{\\><]/.test(value);
+}
+
+const isTelNumValidation = (value) => {
+    return !/^[0-9]*$/.test(value);
+}
+
 const formAddError = (input) => {
-    input.parentElement.classList.add('-error');
-    input.classList.add('-error');
+    console.log("input");
+    console.log(input.value);
+    input.parentElement.classList.add("error");
+    input.classList.add("error");
 }
 
 const formRemoveError = (input) => {
-    input.parentElement.classList.remove('-error');
-    input.classList.remove('-error');
-}
-
-const emailValidation = (input) => {
-    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-}
-
-const nameValidation = (input) => {
-    return /^[0-9,!@#$%^&*)(_/?\-=.`'"|~\]\[₴№;:}{\\><]/.test(input.value);
-}
-
-const telNumValidation = (input) => {
-    return !/^[0-9]*$/.test(input.value);
+    input.parentElement.classList.remove("error");
+    input.classList.remove("error");
 }
